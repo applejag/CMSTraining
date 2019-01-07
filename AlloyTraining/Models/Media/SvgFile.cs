44dc164707ca6bbf15c113874c2bb924bb4d32b0
kalle (jag) <kalle.jillheden@gmail.com>
2019-01-07 15:52:54 +0100

    Added SvgFile media type, with sv translation

---
+++ AlloyTraining/Models/Media/SvgFile.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using EPiServer.Core;
+using EPiServer.DataAbstraction;
+using EPiServer.DataAnnotations;
+using EPiServer.Framework.Blobs;
+using EPiServer.Framework.DataAnnotations;
+
+namespace AlloyTraining.Models.Media
+{
+    [ContentType(DisplayName = "SVG file",
+        GUID = "45031716-a1e5-4795-bb5b-e96123340e52",
+        Description = "Use this to upload Scalable Vector Graphic(SVG) images.")]
+    [MediaDescriptor(ExtensionString = "svg")]
+    public class SvgFile : ImageData
+    {
+        // instead of generating a smaller bitmap file for thumbnail,
+        // use the same binary vector image for thumbnail
+        public override Blob Thumbnail => BinaryData;
+    }
+}
