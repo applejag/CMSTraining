9e2583a60fe8a67624079230bba4d153c2aeaee2
kalle (jag) <kalle.jillheden@gmail.com>
2019-01-08 14:44:35 +0100

    Added footer to root layout

---
+++ AlloyTraining/Business/ExtensionMethods/LayoutExtensionMethods.cs
@@ -0,0 +1,19 @@
+﻿using System.Web.Mvc;
+using AlloyTraining.Models.Pages;
+using AlloyTraining.Models.ViewModels;
+using EPiServer;
+using EPiServer.Core;
+using EPiServer.ServiceLocation;
+
+namespace AlloyTraining.Business.ExtensionMethods
+{
+    public static class LayoutExtensionMethods
+    {
+        public static string GetFooter(this HtmlHelper html)
+        {
+            var loader = ServiceLocator.Current.GetInstance<IContentLoader>();
+            var startPage = loader.Get<StartPage>(ContentReference.StartPage);
+            return startPage?.FooterText;
+        }
+    }
+}
