4aa56292853ef80425292f577b4e74fc1fe59b86
kalle (jag) <kalle.jillheden@gmail.com>
2019-01-08 15:10:29 +0100

    Added standard page type

---
+++ AlloyTraining/Models/Pages/StandardPage.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using EPiServer.Core;
+using EPiServer.DataAbstraction;
+using EPiServer.DataAnnotations;
+using EPiServer.SpecializedProperties;
+
+namespace AlloyTraining.Models.Pages
+{
+    [ContentType(DisplayName = "Standard",
+        GUID = "6f437a1e-661f-48ed-abb2-b42f5e808254",
+        Description = "Use this page type unless you need a more specialized one.",
+        GroupName = SiteGroupNames.Common)]
+    [SitePageIcon]
+    public class StandardPage : SitePageData
+    {
+
+        [CultureSpecific]
+        [Display(
+            Name = "Main body",
+            Description = "The main body will be shown in the main content area of the page, using the XHTML-editor you can insert for example text, images and tables.",
+            GroupName = SystemTabNames.Content,
+            Order = 150)]
+        public virtual XhtmlString MainBody { get; set; }
+
+    }
+}
