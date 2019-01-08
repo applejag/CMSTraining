ebca4c8eb00e4fd439d145c27d07964b9d255ef0
kalle (jag) <kalle.jillheden@gmail.com>
2019-01-08 11:32:23 +0100

    Added SitePageData

---
+++ AlloyTraining/Models/Pages/SitePageData.cs
@@ -0,0 +1,12 @@
+﻿using EPiServer.Core;
+
+namespace AlloyTraining.Models.Pages
+{
+    public abstract class SitePageData : PageData
+    {
+        public string MetaTitle { get; set; }
+        public string MetaKeywords { get; set; }
+        public string MetaDescription { get; set; }
+        public ContentReference PageImage { get; set; }
+    }
+}
