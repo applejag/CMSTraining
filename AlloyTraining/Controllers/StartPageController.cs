b6e13393a1d6ede5a4247e8d05de4c245f9ceaee
kalle (jag) <kalle.jillheden@gmail.com>
2019-01-08 13:47:57 +0100

    Changed StartPage to use the base controller

--- AlloyTraining/Controllers/StartPageController.cs
+++ AlloyTraining/Controllers/StartPageController.cs
@@ -1,14 +1,19 @@
 ﻿using System.Web.Mvc;
 using AlloyTraining.Models.Pages;
+using EPiServer;
 using EPiServer.Web.Mvc;
 
 namespace AlloyTraining.Controllers
 {
-    public class StartPageController : PageController<StartPage>
+    public class StartPageController : PageControllerBase<StartPage>
     {
+        public StartPageController(IContentLoader loader) : base(loader)
+        {
+        }
+
         public ActionResult Index(StartPage currentPage)
         {
-            return View(currentPage);
+            return View(CreatePageViewModel(currentPage));
         }
     }
 }
