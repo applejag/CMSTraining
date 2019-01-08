aba83e04b89ea485d2a84ffa344ae2ef61fa15b3
kalle (jag) <kalle.jillheden@gmail.com>
2019-01-08 13:43:09 +0100

    Added base controller

---
+++ AlloyTraining/Controllers/PageControllerBase.cs
@@ -0,0 +1,45 @@
+﻿using System.Linq;
+using System.Web.Mvc;
+using System.Web.Security;
+using AlloyTraining.Business.ExtensionMethods;
+using AlloyTraining.Models.Pages;
+using AlloyTraining.Models.ViewModels;
+using EPiServer;
+using EPiServer.Core;
+using EPiServer.Filters;
+using EPiServer.Web.Mvc;
+
+namespace AlloyTraining.Controllers
+{
+    public abstract class PageControllerBase<T> : PageController<T> where T : SitePageData
+    {
+        protected readonly IContentLoader Loader;
+
+        protected PageControllerBase(IContentLoader loader)
+        {
+            Loader = loader;
+        }
+
+        public ActionResult Logout()
+        {
+            FormsAuthentication.SignOut();
+            return RedirectToAction("Index");
+        }
+
+        protected IPageViewModel<TPage> CreatePageViewModel<TPage>(
+            TPage currentPage) where TPage : SitePageData
+        {
+            PageViewModel<TPage> viewmodel = PageViewModel.Create(currentPage);
+
+            viewmodel.StartPage = Loader.Get<StartPage>(ContentReference.StartPage);
+            viewmodel.MenuPages = FilterForVisitor
+                .Filter(Loader.GetChildren<SitePageData>(ContentReference.StartPage))
+                .Cast<SitePageData>()
+                .Where(page => page.VisibleInMenu);
+            viewmodel.Section = currentPage.ContentLink.GetSection();
+
+            return viewmodel;
+        }
+
+    }
+}
