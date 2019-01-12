e61179ab1bb1bbf40f9c857497b548dec39aafc2
kalle (jag) <kalle.jillheden@gmail.com>
2019-01-12 21:10:56 +0100

    Added navigation menu, and a page:action url resolver extension method

---
+++ AlloyTraining/Business/ExtensionMethods/UrlHelperExtensions.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Reflection;
+using System.Web.Mvc;
+using AlloyTraining.Controllers;
+using AlloyTraining.Models.Pages;
+using EPiServer;
+using EPiServer.Core;
+using EPiServer.ServiceLocation;
+using EPiServer.Web.Mvc.Html;
+using EPiServer.Web.Routing;
+
+namespace AlloyTraining.Business.ExtensionMethods
+{
+    public static class UrlHelperExtensions
+    {
+        public static string Action(this UrlHelper url, ContentReference contentLink, string action = "index")
+        {
+            var resolver = ServiceLocator.Current.GetInstance<UrlResolver>();
+
+            return resolver.GetUrl(contentLink, null, new VirtualPathArguments
+            {
+                Action = action
+            });
+        }
+    }
+}
