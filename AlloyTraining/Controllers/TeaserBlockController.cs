683cff707965219927507af0d92f642a3cf05fa7
kalle (jag) <kalle.jillheden@gmail.com>
2019-01-14 15:18:48 +0100

    Added Wide and Narrow teaser controllers

--- AlloyTraining/Controllers/TeaserBlockController.cs
+++ AlloyTraining/Controllers/TeaserBlockController.cs
@@ -7,12 +7,47 @@ using AlloyTraining.Models.Blocks;
 using AlloyTraining.Models.ViewModels;
 using EPiServer;
 using EPiServer.Core;
+using EPiServer.Framework.DataAnnotations;
 using EPiServer.Web;
 using EPiServer.Web.Mvc;
 
 namespace AlloyTraining.Controllers
 {
+    [TemplateDescriptor(Tags = new[] { SiteTags.Full },
+        AvailableWithoutTag = true)]
     public class TeaserBlockController : BlockController<TeaserBlock>
+    {
+        public override ActionResult Index(TeaserBlock currentBlock)
+        {
+            var model = new TeaserBlockViewModel
+            {
+                CurrentBlock = currentBlock,
+                TodayVisitorCount = new Random().Next(900, 10900)
+            };
+
+            return PartialView(model);
+        }
+    }
+
+    [TemplateDescriptor(Tags = new[] { SiteTags.Wide },
+        AvailableWithoutTag = false)]
+    public class TeaserBlockWideController : BlockController<TeaserBlock>
+    {
+        public override ActionResult Index(TeaserBlock currentBlock)
+        {
+            var model = new TeaserBlockViewModel
+            {
+                CurrentBlock = currentBlock,
+                TodayVisitorCount = new Random().Next(900, 10900)
+            };
+
+            return PartialView(model);
+        }
+    }
+
+    [TemplateDescriptor(Tags = new[] { SiteTags.Narrow },
+        AvailableWithoutTag = false)]
+    public class TeaserBlockNarrowController : BlockController<TeaserBlock>
     {
         public override ActionResult Index(TeaserBlock currentBlock)
         {
