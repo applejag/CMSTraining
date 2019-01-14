4f4f95797013ffbef516d52e5018036ada8e3d44
kalle (jag) <kalle.jillheden@gmail.com>
2019-01-14 14:04:50 +0100

    Added editorial block type

---
+++ AlloyTraining/Models/Blocks/EditorialBlock.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using EPiServer.Core;
+using EPiServer.DataAbstraction;
+using EPiServer.DataAnnotations;
+
+namespace AlloyTraining.Models.Blocks
+{
+    [ContentType(DisplayName = "Editorial",
+        GUID = "73622091-30ce-4283-a41b-00250ef0cca9",
+        Description = "Use this for a rich editorial text that will be reused in multiple places.")]
+    [SiteBlockIcon]
+    public class EditorialBlock : BlockData
+    {
+        [CultureSpecific]
+        [Display(Name = "Main body",
+            Description = "The main body will be shown in the main content area of the page, using the XHTML-editor you can insert for example text, images and tables.",
+            GroupName = SystemTabNames.Content,
+            Order = 10)]
+        public virtual XhtmlString MainBody { get; set; }
+    }
+}
