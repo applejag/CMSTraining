dc73ad03346c0b80f40be4fcc3ae8873f124600b
kalle (jag) <kalle.jillheden@gmail.com>
2019-01-14 14:39:34 +0100

    Added teaser controller, viewmodel, and view

---
+++ AlloyTraining/Controllers/TeaserBlockController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using AlloyTraining.Models.Blocks;
+using AlloyTraining.Models.ViewModels;
+using EPiServer;
+using EPiServer.Core;
+using EPiServer.Web;
+using EPiServer.Web.Mvc;
+
+namespace AlloyTraining.Controllers
+{
+    public class TeaserBlockController : BlockController<TeaserBlock>
+    {
+        public override ActionResult Index(TeaserBlock currentBlock)
+        {
+            var model = new TeaserBlockViewModel
+            {
+                CurrentBlock = currentBlock,
+                TodayVisitorCount = new Random().Next(300, 900)
+            };
+
+            return PartialView(model);
+        }
+    }
+}
---
+++ AlloyTraining/Models/ViewModels/TeaserBlockViewModel.cs
@@ -0,0 +1,10 @@
+﻿using AlloyTraining.Models.Blocks;
+
+namespace AlloyTraining.Models.ViewModels
+{
+    public class TeaserBlockViewModel
+    {
+        public TeaserBlock CurrentBlock { get; set; }
+        public int TodayVisitorCount { get; set; }
+    }
+}
