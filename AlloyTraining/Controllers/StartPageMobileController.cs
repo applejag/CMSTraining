c8d6dab10dd3e451cc3c6d5bfe014f9c5bf98938
kalle (jag) <kalle.jillheden@gmail.com>
2019-01-14 11:56:29 +0100

    Added start page for mobile

---
+++ AlloyTraining/Controllers/StartPageMobileController.cs
@@ -0,0 +1,23 @@
+﻿using System.Web.Mvc;
+using AlloyTraining.Models.Pages;
+using EPiServer;
+using EPiServer.Framework.DataAnnotations;
+using EPiServer.Framework.Web;
+using EPiServer.Web.Mvc;
+
+namespace AlloyTraining.Controllers
+{
+    [TemplateDescriptor(Tags = new [] { RenderingTags.Mobile },
+        AvailableWithoutTag = false)]
+    public class StartPageMobileController : PageControllerBase<StartPage>
+    {
+        public StartPageMobileController(IContentLoader loader) : base(loader)
+        {
+        }
+
+        public ActionResult Index(StartPage currentPage)
+        {
+            return View(CreatePageViewModel(currentPage));
+        }
+    }
+}
