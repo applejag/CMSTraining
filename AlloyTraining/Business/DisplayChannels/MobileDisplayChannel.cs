122870f22c19dfcf39fbfd6359be48ac20d8150a
kalle (jag) <kalle.jillheden@gmail.com>
2019-01-14 13:49:55 +0100

    Added mobile display resolutions for iPhone 4 and 5 Couldn't get the translation for display resolutions to work though :(

--- AlloyTraining/Business/DisplayChannels/MobileDisplayChannel.cs
+++ AlloyTraining/Business/DisplayChannels/MobileDisplayChannel.cs
@@ -12,5 +12,29 @@ namespace AlloyTraining.Business.DisplayChannels
         }
 
         public override string ChannelName => RenderingTags.Mobile;
+
+        public override string ResolutionId => IPhone5.ResolutionId;
+    }
+
+    // ReSharper disable once InconsistentNaming
+    public class IPhone5 : IDisplayResolution
+    {
+        public const string ResolutionId = "iphone5";
+
+        public string Id => ResolutionId;
+        public string Name => "iPhone 5 (320 x 568)";
+        public int Width => 320;
+        public int Height => 568;
+    }
+
+    // ReSharper disable once InconsistentNaming
+    public class IPhone4 : IDisplayResolution
+    {
+        public const string ResolutionId = "iphone4";
+
+        public string Id => ResolutionId;
+        public string Name => "iPhone 4 (320 x 480)";
+        public int Width => 320;
+        public int Height => 480;
     }
 }
