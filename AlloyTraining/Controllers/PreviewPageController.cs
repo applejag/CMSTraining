8500d935cc515ee102da95065746f245ed3ac35d
kalle (jag) <kalle.jillheden@gmail.com>
2019-01-14 15:34:25 +0100

    Added preview page controller, viewmodel, and views

---
+++ AlloyTraining/Controllers/PreviewPageController.cs
@@ -0,0 +1,33 @@
+﻿using System.Web.Mvc;
+using AlloyTraining.Models.Pages;
+using AlloyTraining.Models.ViewModels;
+using EPiServer;
+using EPiServer.Core;
+using EPiServer.Framework.DataAnnotations;
+using EPiServer.Framework.Web;
+using EPiServer.Web;
+using EPiServer.Web.Mvc;
+
+namespace AlloyTraining.Controllers
+{
+    [TemplateDescriptor(Inherited = true,
+        TemplateTypeCategory = TemplateTypeCategories.MvcController,
+        Tags = new[] { RenderingTags.Preview },
+        AvailableWithoutTag = false)]
+    public class PreviewPageController : ActionControllerBase, IRenderTemplate<BlockData>
+    {
+        private readonly IContentLoader _loader;
+
+        public PreviewPageController(IContentLoader loader)
+        {
+            _loader = loader;
+        }
+
+        public ActionResult Index(IContent currentContent)
+        {
+            var startPage = _loader.Get<StartPage>(ContentReference.StartPage);
+            var model = new PreviewPageViewModel(startPage, currentContent);
+            return View(model);
+        }
+    }
+}
---
+++ AlloyTraining/Models/ViewModels/PreviewPageViewModel.cs
@@ -0,0 +1,17 @@
+﻿using AlloyTraining.Models.Pages;
+using EPiServer.Core;
+
+namespace AlloyTraining.Models.ViewModels
+{
+    public class PreviewPageViewModel : PageViewModel<SitePageData>
+    {
+        public ContentArea PreviewArea { get; set; }
+
+        public PreviewPageViewModel(SitePageData currentPage, IContent contentToPreview)
+            : base(currentPage)
+        {
+            PreviewArea = new ContentArea();
+            PreviewArea.Items.Add(new ContentAreaItem { ContentLink = contentToPreview.ContentLink });
+        }
+    }
+}
