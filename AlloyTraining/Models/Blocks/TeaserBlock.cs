3d491efaba11decb4de1a3f858048dda8a135a1f
kalle (jag) <kalle.jillheden@gmail.com>
2019-01-14 14:24:05 +0100

    Added teaser block type

---
+++ AlloyTraining/Models/Blocks/TeaserBlock.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using EPiServer.Core;
+using EPiServer.DataAbstraction;
+using EPiServer.DataAnnotations;
+using EPiServer.Web;
+
+namespace AlloyTraining.Models.Blocks
+{
+    [ContentType(DisplayName = "Teaser",
+        GUID = "ee0a9da0-534d-4c1e-a22f-abdf6ffaa767",
+        Description = "Use this for rich text with heading, image and page link that will be reused in multiple places")]
+    [SiteBlockIcon]
+    public class TeaserBlock : BlockData
+    {
+        [Display(Name = "Teaser heading", Order = 10)]
+        [CultureSpecific]
+        public virtual string TeaserHeading { get; set; }
+
+        [Display(Name = "Teaser text", Order = 20)]
+        [CultureSpecific]
+        public virtual XhtmlString TeaserText { get; set; }
+
+        [Display(Name = "Teaser thumbnail image", Order = 30)]
+        [UIHint(UIHint.Image)]
+        public virtual ContentReference TeaserImage { get; set; }
+
+        [Display(Name = "Teaser page link", Order = 40)]
+        public virtual PageReference TeaserLink { get; set; }
+    }
+}
