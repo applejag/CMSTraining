aa491b080a30be308d393dd230c7466e4d8f7ab9
kalle (jag) <kalle.jillheden@gmail.com>
2019-01-14 11:59:53 +0100

    Added display channel for mobile

---
+++ AlloyTraining/Business/DisplayChannels/MobileDisplayChannel.cs
@@ -0,0 +1,16 @@
+﻿using System.Web;
+using EPiServer.Framework.Web;
+using EPiServer.Web;
+
+namespace AlloyTraining.Business.DisplayChannels
+{
+    public class MobileDisplayChannel : DisplayChannel
+    {
+        public override bool IsActive(HttpContextBase context)
+        {
+            return context.Request.Browser.IsMobileDevice;
+        }
+
+        public override string ChannelName => RenderingTags.Mobile;
+    }
+}
