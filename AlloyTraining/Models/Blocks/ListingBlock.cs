fe912d9e816ac5808e50fdafa5747235c61f3256
kalle (jag) <kalle.jillheden@gmail.com>
2019-01-15 16:03:56 +0100

    Added listing block model

---
+++ AlloyTraining/Models/Blocks/ListingBlock.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using EPiServer.Core;
+using EPiServer.DataAbstraction;
+using EPiServer.DataAnnotations;
+
+namespace AlloyTraining.Models.Blocks
+{
+    [ContentType(DisplayName = "Listing",
+        GroupName = SiteGroupNames.Common,
+        GUID = "f298a01e-5de1-4fb3-8d6d-9ff9d3deee44",
+        Description = "Choose a page in the tree, and its children will be listed, with a heading.")]
+    public class ListingBlock : BlockData
+    {
+        [Display(Name = "Heading", Order = 10)]
+        public virtual string Heading { get; set; }
+
+        [Display(Name = "Show children of this page", Order = 20)]
+        public virtual PageReference ShowChildrenOfThisPage { get; set; }
+        /*
+                [CultureSpecific]
+                [Display(
+                    Name = "Name",
+                    Description = "Name field's description",
+                    GroupName = SystemTabNames.Content,
+                    Order = 1)]
+                public virtual string Name { get; set; }
+         */
+    }
+}
