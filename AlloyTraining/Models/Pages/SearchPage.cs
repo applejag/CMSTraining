98993af0ddb95e39d5db4791da5b3ae664d827c0
kalle (jag) <kalle.jillheden@gmail.com>
2019-01-18 16:00:39 +0100

    Added search page and viewmodel

---
+++ AlloyTraining/Models/Pages/SearchPage.cs
@@ -0,0 +1,14 @@
+﻿using EPiServer.Core;
+using EPiServer.DataAnnotations;
+
+namespace AlloyTraining.Models.Pages
+{
+    [ContentType(DisplayName = "Search",
+        GroupName = SiteGroupNames.Specialized, Order = 30,
+        GUID = "044d7070-a9fa-4efb-822a-882435a29a09",
+        Description = "Use this to enable visitors to search for pages and media on the site.")]
+    [SitePageIcon]
+    public class SearchPage : SitePageData
+    {
+    }
+}
