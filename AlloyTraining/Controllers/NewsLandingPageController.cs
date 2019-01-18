647b1281a336c7586a93b63fe495fdbc0cd09de2
kalle (jag) <kalle.jillheden@gmail.com>
2019-01-18 14:50:29 +0100

    Added landing page type, controller and view

---
+++ AlloyTraining/Controllers/NewsLandingPageController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Web.Mvc;
+using AlloyTraining.Models.Pages;
+using EPiServer;
+using EPiServer.Core;
+using EPiServer.Framework.DataAnnotations;
+using EPiServer.Web.Mvc;
+
+namespace AlloyTraining.Controllers
+{
+    public class NewsLandingPageController : PageControllerBase<NewsLandingPage>
+    {
+        public NewsLandingPageController(IContentLoader loader) : base(loader)
+        {
+        }
+
+        public ActionResult Index(NewsLandingPage currentPage)
+        {
+            return View(CreatePageViewModel(currentPage));
+        }
+    }
+}
---
+++ AlloyTraining/Models/Pages/NewsLandingPage.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using AlloyTraining.Models.Blocks;
+using EPiServer.Core;
+using EPiServer.DataAbstraction;
+using EPiServer.DataAnnotations;
+using EPiServer.SpecializedProperties;
+
+namespace AlloyTraining.Models.Pages
+{
+    [ContentType(DisplayName = "News Landing", 
+        GUID = "5914bc4a-0301-4323-9359-06d94626e3ed",
+        Description = "Use this as a landing page for a list of news articles.",
+        GroupName = SiteGroupNames.News)]
+    [SitePageIcon]
+    public class NewsLandingPage : StandardPage
+    {
+        [Display(Name = "News listing",
+            Order = 315,
+            GroupName = SystemTabNames.Content)]
+        public virtual ListingBlock NewsListing { get; set; }
+    }
+}
